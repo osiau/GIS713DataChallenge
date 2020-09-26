2535c613f2522288ced97ec0771a7e4323459366
asaffer <asaffer@ncsu.edu>
2020-09-26 13:00:57 -0400

    Add files via upload
    Two files:
    script used to create crosswalk county and state name/fips/geoID ( output now saved as all_names_FIPS.csv)
    cleaned water quality data with state and county fips and geoID

---
+++ air_quality_clean.R
@@ -0,0 +1,35 @@
+
+# THIS SCRIPT gives you clean air quality data (not clean air quality though, unfortunately)
+# The final clean data objects are: air_qual2019fp and air_qual2020_fp
+# No calculation has been done on the air quality fields
+
+library(data.table)
+wd <- getwd()
+
+# read
+
+air_qual2019 <- read.csv(file.path(wd,"Datasources/air_quaity_annual_aqi_by_county_2019.csv"),stringsAsFactors=TRUE, header=TRUE)
+air_qual2020 <- read.csv(file.path(wd,"Datasources/air_quality_annual_aqi_by_county_2020.csv"),stringsAsFactors=TRUE, header=TRUE)
+data_states <- read.csv(file.path(wd,"Datasources/all_names_FIPS.csv"),stringsAsFactors=TRUE)
+
+# convert to data.table
+
+air_qual2019 <- as.data.table(air_qual2019)
+air_qual2020 <- as.data.table(air_qual2020)
+
+# lowercase names
+
+names(air_qual2019) <- tolower(names(air_qual2019))
+names(air_qual2020) <- tolower(names(air_qual2020))
+
+summary(data_states)
+
+# adding codes to air quality
+
+air_qual2019$state <- tolower(air_qual2019$state)
+air_qual2019$county <- tolower(air_qual2019$county)
+air_qual2019 <- merge(air_qual2019,data_states,by=c("state","county"))
+
+air_qual2020$state <- tolower(air_qual2020$state)
+air_qual2020$county <- tolower(air_qual2020$county)
+air_qual2020 <- merge(air_qual2020,data_states,by=c("state","county"))
---
+++ county_state_fips.R
@@ -0,0 +1,23 @@
+library(rgdal)
+library(data.table)
+wd <- getwd()
+
+# File to create the all_names_FIPS.csv crosswalk
+
+states <- readOGR(file.path(wd,"Datasources/cb_2015_us_county_20m.shp"), stringsAsFactors=TRUE)
+states_fips <-read.csv(file.path(wd,"Datasources/state-geocodes-v2017.csv"),stringsAsFactors=TRUE)
+
+data_states <- as.data.table(states@data)
+states_fips <- as.data.table(states_fips)
+
+names(states_fips) <- c("region","div","stateFIPS","state")
+names(data_states) <- c("stateFIPS","countyFIPS","NS","AFF","geoID","county","lsad","aland","awater")
+
+states_fips$stateFIPS <- as.factor(formatC(states_fips$stateFIPS,width=2,flag="0"))
+
+data_states <- merge(data_states,states_fips,by="stateFIPS", all.x=TRUE)
+data_states <- data_states[,.(stateFIPS,countyFIPS,geoID,county,state)]
+data_states$county <- tolower(data_states$county)
+data_states$state <- tolower(data_states$state)
+
+# write.csv(data_states,file.path(wd,"Datasources/all_names_FIPS.csv")) 
