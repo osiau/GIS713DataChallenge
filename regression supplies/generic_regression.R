646bb61ae489aa1190b052b25d7632d2480d5806
asaffer <asaffer@ncsu.edu>
2020-09-27 18:50:08 -0400

    now with state and county options

--- regression supplies/generic_regression.R
+++ regression supplies/generic_regression.R
@@ -1,12 +1,16 @@
 # Generic regressions script
+# Toggle options for state vs. county (county is default)
 
 data_independent <- fread(file.path(wd,"Datasources/file.csv"),stringsAsFactors=TRUE) # change to csv/rds name
-data_dependent <- data_dependent <- readRDS(file.path(wd,"Datasources/dependent_vars.RDS"))
+data_dependent <- data_dependent <- readRDS(file.path(wd,"Datasources/county_dependent_vars.RDS")) #for county
+# data_dependent <- data_dependent <- readRDS(file.path(wd,"Datasources/state_dependent_vars.RDS")) # for state
 
-## if your data is not named properly, you may need to rename something to geoID:
-# setnames(data, "oldname", "geoID")
+## if your data is not named properly, you may need to rename something to geoID/stateFIPS:
+# setnames(data, "oldname", "geoID") - for county
+# setnames(data, "oldname", "stateFIPS") - for state
 
-dataset <- merge(data_independent, data_dependent, by="geoID", all.x=TRUE)
+dataset <- merge(data_independent, data_dependent, by="geoID", all.x=TRUE) # for county
+# dataset <- merge(data_independent, data_dependent, by="stateFIPS", all.x=TRUE) # for state
 
 y <- datatset$dependent # change to variable name
 x <- dataset$independent # change to variable name
