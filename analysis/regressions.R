0346ad433e348ca34070fcc5d40afddf26118cc0
blaginh <blaginh@ncsu.edu>
2020-09-27 12:38:34 -0400

    regression harmonization pseudocode

---
+++ analysis/regressions.R
@@ -0,0 +1,16 @@
+##Assign blue vs. red winner to county and covid cases and deaths)])
+##Assign blue vs. red to state
+#Import covid data cases, deaths
+#Import unemployment data
+##Cases, deaths at end date county level
+##Cases, deaths at end date state level
+##Cases, deaths at peak date county level
+##Cases, deaths at peak date state level
+#box plots - covid case data blue vs. red (county & state end point & peak)
+##lm, glm (beta)
+##check Q-Q plots and Cook's distance
+#box plots - covid case data blue vs. red (county & state end point & peak)
+##lm, glm (beta)
+##check Q-Q plots and Cook's distance
+
+
